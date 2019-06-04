8b3ad2286a52f563ab048c6c0a4183c2841ac287
Pedro Rocha <p.rocha1990@gmail.com>
2019-06-04 16:11:47 -0400

    include documentation for SoundShape using roxygen2

---
+++ R/SoundShape.R
@@ -0,0 +1,33 @@
+#' Sound Waves Onto Morphometric Data
+#'
+#'
+#' @description
+#' A set of functions that crosses the bridge between Bioacoustics and Geometric Morphometrics.
+#'
+#' \itemize{
+#'   \item{\code{\link{align.wave}}: Automatic placement of calls at beggining of sound window}
+#'   \item{\code{\link{eigensound}}: Calculate spectrogram data for each \code{wave} file on a given folder and acquire semilandmarks using a three-dimensional representation of sound or the cross-correlation between energy quantiles and a curve of relative amplitude}
+#'   \item{\code{\link{pca.plot}}: Plot ordination of Principal Components with convex hulls}
+#' }
+#'
+#' @docType package
+#' @name SoundShape
+"_PACKAGE"
+#'
+#' @seealso
+#' \code{\link{geomorph}}, \code{\link{seewave}}
+#'
+#' Useful links:
+#' \itemize{
+#'  \item{\url{https://github.com/p-rocha/SoundShape}}
+#'  \item{Report bugs at \url{https://github.com/p-rocha/SoundShape/issues}}}
+#'  }
+#'
+#'  @author
+#'  Pedro Rocha \email{p.rocha1990@gmail.com}
+#'
+#'  @references
+#'  Rocha, P. & Romano, P. (\emph{in prep}) The shape of sound: A new \code{R} package that crosses the bridge between Geometric Morphometrics and Bioacoustics.
+#'
+#'  @export
+#'
