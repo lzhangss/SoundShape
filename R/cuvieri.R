f5038b373f44eec57509e7e67b20871a3905e0a6
Pedro Rocha <p.rocha1990@gmail.com>
2019-06-05 13:11:50 -0400

    add help files for wave data

---
+++ R/cuvieri.R
@@ -0,0 +1,30 @@
+#' Advertisement call of \emph{Physalaemus cuvieri}
+#'
+#' @description
+#' Recording of \emph{Physalaemus cuvieri} advertisement call containing three notes emitted in a sequence. Edited from original \code{Wave} file for optimal sinal to noise ratio and reduced time duration.
+#'
+#' @docType data
+#'
+#' @usage
+#' data(cuvieri)
+#'
+#' @details
+#' Duration = 2.37 s. Sampling Frequency = 22050 Hz. Air temperature 22ºC.
+#'
+#' Recorded at São José dos Campos Municipality, São Paulo State, Brazil, on 24 September 2013.
+#'
+#' @format
+#' An object of the class \code{"Wave"} (\code{\link{tuneR}} package.
+#'
+#' @source
+#' Original recording housed at Coleção Bioacústica da Universidade Federal de Minas Gerais (CBUFMG-00196). Recorded by Pedro Rocha.
+#'
+#' @examples
+#' data(cuvieri)
+#'
+#' seewave::oscillo(cuvieri, f=22050)
+#' seewave::spectro(cuvieri, f=22050)
+#'
+#' @export
+#'
+
