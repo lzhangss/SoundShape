7c2e9716530d2437510b7b9af570189b19f051b0
Pedro Rocha <p.rocha1990@gmail.com>
2019-06-13 13:41:49 -0400

    insert file codes for threeDspectro

---
+++ Codes for threeDspectro.R
@@ -0,0 +1,29 @@
+# devtools and seewave - In case you don't have it
+install.packages("devtools")
+install.packages("seewave")
+
+# SoundShape - Install the development version from GitHub
+devtools::install_github("p-rocha/SoundShape")
+
+# Sample data from SoundShape
+data(cuvieri)
+
+# Select stereotyped acoustic unit from sample
+cuvieri.cut <- seewave::cutw(cuvieri, f=44100, from = 0.05, to=0.45, output="Wave")
+
+# a) Oscillogram
+seewave::oscillo(cuvieri.cut)
+
+# b) 2D spectrogram
+seewave::spectro(cuvieri.cut, flim=c(0, 2.5), grid=FALSE, scale=FALSE)
+
+# c) 3D spectrogram
+threeDspectro(cuvieri.cut, flim=c(0, 2.5), rotate.Xaxis=60, rotate.Yaxis=40)
+
+# d) 3D spectrogram - sampled surface
+threeDspectro(cuvieri.cut, samp.grid=TRUE, x.length=70, y.length=50,
+              flim=c(0, 2.5), rotate.Xaxis=60, rotate.Yaxis=40)
+
+# e) 3D spectrogram - semilandmarks from sampled surface
+threeDspectro(cuvieri.cut, samp.grid=TRUE, x.length=70, y.length=50, plot.type="points",
+              flim=c(0, 2.5), rotate.Xaxis=60, rotate.Yaxis=40)
